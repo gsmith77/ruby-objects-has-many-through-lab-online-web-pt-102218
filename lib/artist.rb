64c0b8c2606de5757e3bcdeb3f5dd0c5c0826748
grant smith <grantsmith1256@gmail.com>
2018-11-26 23:29:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,31 +2,7 @@ require "pry"
 
 class Artist
   
-  attr_accessor :name
   
-  @@all_artists = []
   
-   def initialize(name)
-    @name = name
-    @@all_artists << self
-  end
-  
-  def new_song(name, genre)
-    Song.new(name, self, genre)
-  end
-  
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-  
- def genres
-    songs.map(&:genre)
-  end
-  
-   def self.all
-    @@all_artists
-  end
-  
+ 
 end
