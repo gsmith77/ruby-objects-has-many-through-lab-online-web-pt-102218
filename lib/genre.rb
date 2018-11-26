461093a59646fd8e9748fe56b49c55e450df0a9b
grant smith <grantsmith1256@gmail.com>
2018-11-26 23:36:07 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,2 +1,19 @@
 class Genre
+  
+  attr_accessor :name, :song
+  
+  @@all = []
+  
+  def initialize(name, song)
+    @name = name
+    @song = song
+    @@all << self
+  end
+    
+  def self.all
+    @@all 
+  end
+    
+    
+    
 end
